e176e55a107d55846bcf4e18bf20438904d7295d
David McNamara <me@mackers.com>
2012-02-15 23:57:53 +0100

    support for detail view

--- data/AddonsManagerMod.js
+++ data/AddonsManagerMod.js
@@ -35,7 +35,14 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-console.log("in addonsManagerMod");
+//console.log("in addonsManagerMod");
+
+ACRController = {};
+
+self.port.on("init", function(data) {
+
+    ACRController.exclamationImageURL = data.exclamationImageURL;
+});
 
 /*
 function loaded() 
@@ -53,45 +60,74 @@ function loaded()
 }
 */
 
-ACRController = {};
-
 ACRController.onViewChanged = function()
 {
     console.log("in view changed: " + gViewController.currentViewId);
     //console.log("addon count: " + document.getElementById("addon-list").itemCount);
 
-    for (var i=0; i<gViewController.currentViewObj._listBox.itemCount; i++)
+    if (gViewController.currentViewObj._listBox) 
     {
-        var elem = gViewController.currentViewObj._listBox.getItemAtIndex(i);
+        for (var i=0; i<gViewController.currentViewObj._listBox.itemCount; i++)
+        {
+            var elem = gViewController.currentViewObj._listBox.getItemAtIndex(i);
 
-        if (!elem
-            || elem.getAttribute("remote") == "true"
-            || elem.getAttribute("plugin") == "true"
-            || elem.getAttribute("lwtheme") == "true"
-            || elem.getAttribute("type") == "plugin")
-            continue;
+            if (!elem
+                || elem.getAttribute("remote") == "true"
+                || elem.getAttribute("plugin") == "true"
+                || elem.getAttribute("lwtheme") == "true"
+                || elem.getAttribute("type") == "plugin")
+                continue;
 
-        self.port.emit("acr_have_addon", elem.getAttribute("value"));
+            self.port.emit("acr_have_addon", elem.getAttribute("value"));
+        }
+    }
+    else if (gDetailView._addon)
+    {
+        console.log(gDetailView._addon.id);
+        self.port.emit("acr_have_addon", gDetailView._addon.id);
     }
 }
 
 ACRController.makeButtonUI = function(addonReport)
 {
-    // TODO: make nolongerworks UI instead if user has already submitted
-
-    var button = document.createElement("button");
-    button.setAttribute("label", "Report Incompatibility");
-    button.setAttribute("type", "menu");
-    button.setAttribute("class", "anon-control");
-
-    //button.addEventListener("click", function() { ACRController.openSendReportDialog(addonReport); }, true);
-    button.addEventListener("click", function()
+    if (addonReport.state == 0)
     {
-        //ACRController.openSendReportDialog(addonReport);
-        self.port.emit("acr_open_submit_report_dialog", addonReport);
-    }, true);
+        var button = document.createElement("button");
+        button.setAttribute("label", "Report Incompatibility");
+        button.setAttribute("type", "menu");
+        button.setAttribute("class", "anon-control");
+        button.setAttribute("owner", "acr");
 
-    return button;
+        //button.addEventListener("click", function() { ACRController.openSendReportDialog(addonReport); }, true);
+        button.addEventListener("click", function()
+        {
+            //ACRController.openSendReportDialog(addonReport);
+            self.port.emit("acr_open_submit_report_dialog", addonReport);
+        }, true);
+
+        return button;
+    }
+    else if (addonReport.state == 1)
+    {
+        // addon report cannot be "marked as compatible" in this version of the ACR
+        console.error("Addon report in unsupported state (1)");
+    }
+    else if (addonReport.state == 2)
+    {
+        var hbox = document.createElement("hbox");
+        hbox.setAttribute("align", "center");
+        var image = document.createElement("image");
+        image.setAttribute("width", "16");
+        image.setAttribute("height", "16");
+        image.setAttribute("src", this.exclamationImageURL);
+        hbox.appendChild(image);
+        var label = document.createElement("label");
+        label.setAttribute("value", "Compatibility Problems");
+        hbox.appendChild(label);
+
+        return hbox;
+    }
+
 }
 
 /*
@@ -113,26 +149,42 @@ document.addEventListener("ViewChanged", ACRController.onViewChanged, true);
 
 self.port.on("acr_have_addon_report", function(addonReport) {
 
-    console.log("[worker] Add-on '" + addonReport.guid + "/" + addonReport.version + "' state: '"
+    /*console.log("[worker] Add-on '" + addonReport.guid + "/" + addonReport.version + "' state: '"
         + addonReport.state + "' compatibility: " + (addonReport.compatible?"IS":"IS NOT")
-        + " compatible with this version of the platform.");
+        + " compatible with this version of the platform.");*/
     
-    for (var i=0; i<gViewController.currentViewObj._listBox.itemCount; i++)
-    {
-        var elem = gViewController.currentViewObj._listBox.getItemAtIndex(i);
+    var ACRUI = ACRController.makeButtonUI(addonReport);
 
-        if (elem.getAttribute("value") == addonReport.guid) 
+    if (gViewController.currentViewObj._listBox) 
+    {
+        for (var i=0; i<gViewController.currentViewObj._listBox.itemCount; i++)
         {
-            if (gViewController.viewPort.selectedPanel.id == "list-view")
+            var elem = gViewController.currentViewObj._listBox.getItemAtIndex(i);
+
+            if (elem.getAttribute("value") == addonReport.guid) 
             {
                 var controlContainer = document.getAnonymousElementByAttribute(elem, 'anonid', 'control-container');
-                controlContainer.insertBefore(ACRController.makeButtonUI(addonReport), controlContainer.firstChild);
-            }
-            else if (gViewController.viewPort.selectedPanel.id == "details-view")
-            {
-                // TODO
+
+                var existingACRUI = controlContainer.getElementsByAttribute("owner", "acr");
+
+                if (existingACRUI.length)
+                    controlContainer.replaceChild(ACRUI, existingACRUI.item(0));
+                else
+                    controlContainer.insertBefore(ACRUI, controlContainer.firstChild);
             }
         }
+    }
+    else if (gViewController.viewPort.selectedPanel.id == "detail-view")
+    {
+        var existingACRUI = document.getElementById("detail-view").getElementsByAttribute("owner", "acr");
+
+        if (existingACRUI.length)
+            existingACRUI.item(0).parentNode.removeChild(existingACRUI.item(0));
+
+        if (document.getElementById("detail-uninstall"))
+            document.getElementById("detail-uninstall").parentNode.insertBefore(ACRUI, document.getElementById("detail-uninstall"));
+        else if (document.getElementById("detail-enable-btn"))
+            document.getElementById("detail-enable-btn").parentNode.insertBefore(ACRUI, document.getElementById("detail-enable-btn"));
     }
 
 
